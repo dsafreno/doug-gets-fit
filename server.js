75f89790518472c6bea9b924f4754d31a028da7a
Douglas Safreno <dsafreno@cs.stanford.edu>
2014-10-12 23:09:27 -0700

    Better for firebase

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var path = require('path');
 var express = require('express');
 var app = express();
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/doug-gets-fit.html');
+    res.sendFile(__dirname + '/index.html');
 });
 app.use(express.static(__dirname));
 var server = app.listen(3000, function() {
